b590b33d652a9e301f259357644798292154c95e
LyovkinIlya <lyovkin2001@mail.ru>
2024-09-24 12:22:58 +0300

    23/09

---
+++ module_1_2.py
@@ -0,0 +1,5 @@
+number_homework = 12
+number_of_hours_spent = 1.5
+course_name = "Python"
+time_for_one_task = number_of_hours_spent / number_homework
+print('Курс:', course_name + ',' ,'всего задач:', str(number_homework) + ',', 'затрачено часов:', str(number_of_hours_spent) + ',', 'среднее время выполнения', str(time_for_one_task) + '.')
