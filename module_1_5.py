2559a8273b00fe45b765b5de2133bc3388a80526
LyovkinIlya <lyovkin2001@mail.ru>
2024-09-26 12:39:30 +0300

    26.09

---
+++ module_1_5.py
@@ -0,0 +1,16 @@
+immutable_var = (1, True, 'moon', 2.5)
+print(immutable_var)
+immutable_var[0] = 2
+immutable_var[1] = False
+immutable_var[2] = 'sun'
+immutable_var[3] = 5.0
+print(immutable_var)
+# При попытке изменить элементы кортежа консоль выдаёт ошибку:
+# 'tuple' object does not support item assignment, означающую, что
+# кортеж относится к неизменяемым типам данных.
+mutable_list = [1, True, 'moon', 2.5]
+mutable_list[0] = 2
+mutable_list[1] = False
+mutable_list[2] = 'sun'
+mutable_list[3] = 5.0
+print(mutable_list)
