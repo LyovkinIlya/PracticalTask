b27d37b066dfb2d2a4968d30eb639bedb6fa282a
LyovkinIlya <lyovkin2001@mail.ru>
2024-09-27 13:46:28 +0300

    27.09

--- module_1_6.py
+++ module_1_6.py
@@ -1,16 +1,16 @@
 #Словарь:
 my_dict = {'Ilya': 2001, 'Danil': 2002, 'Roman': 2000}
-print('Dict:', my_dict)
-print('Existing value:', my_dict['Ilya'])
-print('Not existing value:', my_dict.get('Max'))
-my_dict.update({'Diana': 1999, 'Maria': 1998})
-del_val = my_dict.pop('Roman')
-print('Deleted value:', del_val)
-print('Modified dictionary:', my_dict)
+print('Dict:', my_dict)                          #словарь my_dict
+print('Existing value:', my_dict['Ilya'])        #одно значение по существующему ключу
+print('Not existing value:', my_dict.get('Max')) #одно по отсутствующему ключу
+my_dict.update({'Diana': 1999, 'Maria': 1998})   #две новые пары
+del_val = my_dict.pop('Roman')                   #удаление одной пары
+print('Deleted value:', del_val)                 #вывод удаленной пары
+print('Modified dictionary:', my_dict)           #измененный словарь my_dict
 #Множество:
 my_set = {100, True, True, 'book', 100, 'book'}
-print('Set:', my_set)
-my_set.add((1, 2))
-my_set.add(0.5)
-my_set.discard(True)
-print('Modified set:', my_set)
+print('Set:', my_set)                            #множество my_set
+my_set.add((1, 2))                               # два новых..
+my_set.add(0.5)                                  # ..элемента
+my_set.discard(True)                             #удаление элемента
+print('Modified set:', my_set)                   #измененное множество my_set
