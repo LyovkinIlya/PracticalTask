f3514837e65d8f906073043f5e516791c8e5f78e
LyovkinIlya <lyovkin2001@mail.ru>
2024-10-03 16:47:18 +0300

    03.10 + comments

--- module_2_4.py
+++ module_2_4.py
@@ -1,16 +1,16 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
-for i in numbers:
-    if i > 1:
-        is_prime = True
-        for j in range(2, i):
-            if i % j == 0:
+for i in numbers:                # Перебираем список numbers
+    if i > 1:                    # Исключаем 1 из списка
+        is_prime = True          # flag
+        for j in range(2, i):    # Перебираем j от 2 до i(не включительно)
+            if i % j == 0:       # Проверка на простое число
                 is_prime = False
                 break
         if is_prime:
-            primes.append(i)
+            primes.append(i)     # Дополняем список простыми числами
         else:
-            not_primes.append(i)
+            not_primes.append(i) # Дополняем список Не простыми числами
 print(primes)
 print(not_primes)
