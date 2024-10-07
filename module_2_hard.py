5dcebc7d426dc4e0af0f3f7d6d5586516f0637f2
LyovkinIlya <lyovkin2001@mail.ru>
2024-10-07 13:36:04 +0300

    07.10 с условием n от 3 до 20

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,25 +1,28 @@
 def formatting(list_):
-    list_un = []                               # Убирает вложенные списки
+    list_un = []                                   # Убирает вложенные списки
     for k in list_:
         list_un.extend(k)
 
-    str_ = str(list_un)                        # Готовый список в строку
-    str_ = str_.replace("[", "", ) # Убираем из строки всё лишнее,
-    str_ = str_.replace("]", "", ) # форматируя ее в нужный вид,
-    str_ = str_.replace(" ", "", ) # состоящий только из цифр без
-    str_ = str_.replace(",", "", ) # запятых и т.д.
+    str_ = str(list_un)                            # Готовый список в строку
+    str_ = str_.replace("[", "", )     # Убираем из строки всё лишнее,
+    str_ = str_.replace("]", "", )     # форматируя ее в нужный вид,
+    str_ = str_.replace(" ", "", )     # состоящий только из цифр без
+    str_ = str_.replace(",", "", )     # запятых и т.д.
     print(f'Пароль для {n}: {str_}')
 
 n = int(input('Введите число из первой вставки >> '))
-list_1_20 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-list_result = []
-for a in list_1_20:                            # Получаем список с вложенными парами чисел,
-    for b in list_1_20:                        # подходящие под условия
-        if a != b and n % (a + b) == 0:
-                list_result.append([a, b])
-for i in list_result:                          # Оставляем только уникальные пары чисел
-    for j in list_result:
-        if i == j[::-1]:
-            list_result.remove(j)
+if 3 <= n <= 20:
+    list_1_20 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    list_result = []
+    for a in list_1_20:                            # Получаем список с вложенными парами чисел,
+        for b in list_1_20:                        # подходящие под условия
+            if a != b and n % (a + b) == 0:
+                    list_result.append([a, b])
+    for i in list_result:                          # Оставляем только уникальные пары чисел
+        for j in list_result:
+            if i == j[::-1]:
+                list_result.remove(j)
 
-formatting(list_result)                        # Выводим строку с ответом без лишних знаков
+    formatting(list_result)                        # Выводим строку с ответом без лишних знаков
+else:
+    print('Укажите число от 3 до 20')
