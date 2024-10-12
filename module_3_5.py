a9d9145e471661874fc5050863d255b9a8710792
LyovkinIlya <lyovkin2001@mail.ru>
2024-10-12 09:06:34 +0300

    12.10

--- module_3_5.py
+++ module_3_5.py
@@ -5,6 +5,5 @@ def get_multiplied_digits(number):
         return first * get_multiplied_digits(int(str_number[1:]))
     else:
         return first
-
 result = get_multiplied_digits(40203)
 print(result)
